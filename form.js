f83c340436e1f79417731e3be4f691752cc4a043
Ewan <vulcanv6@hotmail.com>
2018-01-10 16:28:33 +0000

    Create form.js

---
+++ form.js
@@ -0,0 +1,39 @@
+function onFormSubmit(e) {
+
+var fields = [];
+  
+  if (e.response.getItemResponses().length < 3) { 
+    return;
+  }
+
+for (i = 0; i < e.response.getItemResponses().length; i++) {
+    var response = e.response.getItemResponses()[i];
+    fields.push({
+        "name": response.getItem().getTitle(),
+        "value": response.getResponse(),
+        "inline": false
+    });
+}
+
+var data = {
+        "content": '<I added a role mention here to get attention of a specific group of ppl>',
+        "embeds": [{
+            "title": "**New form submission** — " + (e.source.getTitle() != null && e.source.getTitle().length > 0 ? e.source.getTitle() : "Untitled Form"),
+            "type": "rich",
+            "fields": fields
+        }]
+    };
+
+var options = {
+    method: "post",
+    payload: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    muteHttpExceptions: true
+};
+
+Logger.log("Attempting to send:");
+Logger.log(JSON.stringify(data));
+
+var response = UrlFetchApp.fetch("WEBHOOK_URL", options);   //Replace webhook url here
+Logger.log(response.getContentText());
+};
